3b229327f17ef359c19fad5bab9ba7117a7f63c2
Firelods <clement.lefevre1@etu.unice.fr>
2024-09-28 15:40:54 +0200

    :bug: Add 2 missing attributes

--- src/main/java/team/k/Restaurant.java
+++ src/main/java/team/k/Restaurant.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -12,6 +14,8 @@ public class Restaurant {
     private int id;
     private String open;
     private String close;
+//    private List<TimeSlot> timeSlots;  TODO: à décommenter une fois la classe TimeSlot créée
+//    private List<Dish> dishes;  TODO: à décommenter une fois la classe Dish créée
 
     public boolean isAvailable(){
         return true; // TODO: missing this implementation
