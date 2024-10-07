813ba136d5c23026dd9798ace9531b78bcfd53f1
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-10-07 13:19:55 +0200

    :bulb: add comments in OrderStatus

--- src/main/java/team/k/enumerations/OrderStatus.java
+++ src/main/java/team/k/enumerations/OrderStatus.java
@@ -1,5 +1,11 @@
 package team.k.enumerations;
 
 public enum OrderStatus {
-    CREATED, PAID, PLACED, DELIVERING, COMPLETED, DISCOUNT_USED, CANCELED;
+    CREATED, // Dishes can be added or removed, can be payed
+    PAID, // Dishes can't be added or removed, individual orders can never be in this state, they become PLACED directly
+    PLACED, // The restaurant has received the order
+    DELIVERING, // The order is on its way to the customer
+    COMPLETED, // The order has been delivered
+    DISCOUNT_USED, // The order has been used in a discount computation
+    CANCELED // The order has been canceled
 }
