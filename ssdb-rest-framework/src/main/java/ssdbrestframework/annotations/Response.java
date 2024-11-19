0ab3c8c18666c561438a9a188746dc169e905d15
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-11-19 19:13:58 +0100

    :memo: update documentation #119

--- ssdb-rest-framework/src/main/java/ssdbrestframework/annotations/Response.java
+++ ssdb-rest-framework/src/main/java/ssdbrestframework/annotations/Response.java
@@ -3,8 +3,21 @@ package ssdbrestframework.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Annotation allowing to specify the response status if the method is called successfully,
+ * and an optional message to be sent in the response body if the method is void
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Response {
+    /**
+     * The status code to return in the response
+     * @return the status code
+     */
     int status();
+
+    /**
+     * The message to return in the response body if the method is void
+     * @return the message
+     */
     String message() default "";
 }
