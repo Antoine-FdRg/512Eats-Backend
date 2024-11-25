c0aeb31082a52027233a68a1142bf532ed00c2aa
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2024-11-25 21:55:55 +0100

    :white_check_mark: ajout méthode PATCH  #112

--- seinkSansDoozeEats/src/main/java/team/k/orderService/OrderApi.java
+++ seinkSansDoozeEats/src/main/java/team/k/orderService/OrderApi.java
@@ -13,33 +13,6 @@ public class OrderApi {
 
     public static void main(String[] args) {
         SSDBHttpServer serv = new SSDBHttpServer(8086, "team.k.orderService");
-
-        GroupOrderRepository groupOrderRepository = new GroupOrderRepository();
-        LocationRepository locationRepository = new LocationRepository();
-        SubOrderRepository subOrderRepository = new SubOrderRepository();
-        RestaurantRepository restaurantRepository = new RestaurantRepository();
-        RegisteredUserRepository registeredUserRepository = new RegisteredUserRepository();
-        PaymentProcessor paymentProcessor = new PaymentProcessor();
-
-
-        OrderService orderService = new OrderService(
-                groupOrderRepository,
-                locationRepository,
-                subOrderRepository,
-                restaurantRepository,
-                registeredUserRepository,
-                paymentProcessor
-        );
-
-        //TODO : vérifier que c'est utile
-        OrderController orderController = new OrderController(orderService);
-        try {
-            serv.registerController(OrderController.class);
-        } catch (RuntimeException e) {
-            System.err.println("Erreur lors de l'enregistrement du contrôleur : " + e.getMessage());
-            e.printStackTrace();
-        }
-
         serv.start();
     }
 }
