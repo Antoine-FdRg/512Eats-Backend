54416eb16f58051575e527dfa6cb295637c2feb9
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2024-11-26 18:10:21 +0100

    :white_check_mark: individual repo skeleton  #136

--- common-library/src/main/java/commonlibrary/repository/IndividualOrderRepository.java
+++ common-library/src/main/java/commonlibrary/repository/IndividualOrderRepository.java
@@ -1,31 +1,72 @@
 package commonlibrary.repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import commonlibrary.model.Dish;
 import commonlibrary.model.order.IndividualOrder;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
 public class IndividualOrderRepository {
 
-    private final List<IndividualOrder> individualOrders = new ArrayList<>();
+    private static final String BASE_URL = "http://localhost:8082/individual-orders";
+    private static final HttpClient client = HttpClient.newHttpClient();
 
-    public void add(IndividualOrder individualOrder) {
-        individualOrders.add(individualOrder);
+
+    public void add(IndividualOrder individualOrder) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/create"))
+                .POST(HttpRequest.BodyPublishers.ofString(new ObjectMapper().writeValueAsString(individualOrder)))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() >= 300) {
+            throw new IOException("Error: " + response.statusCode() + " - " + response.body());
+        }
     }
 
-    public List<IndividualOrder> findAll() {
-        return individualOrders;
+    public List<IndividualOrder> findAll() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        if (response.statusCode() >= 300) {
+            throw new IOException("Error: " + response.statusCode() + " - " + response.body());
+
+        }
+        List<IndividualOrder> orders = List.of(new ObjectMapper().readValue(response.body(), IndividualOrder[].class)); // throws exception if response is not a valid JSON array
+        return orders;
     }
 
-    public IndividualOrder findById(int id) {
-        return individualOrders.stream()
-                .filter(individualOrder -> individualOrder.getId() == id)
-                .findFirst()
-                .orElse(null);
+    public IndividualOrder findById(int id) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "get" + id))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        if (response.statusCode() >= 300) {
+            throw new IOException("Error: " + response.statusCode() + " - " + response.body());
+
+        }
+        IndividualOrder order = new ObjectMapper().readValue(response.body(), IndividualOrder.class); // throws exception if response is not a valid JSON array
+        return order;
     }
 
-    public void delete(IndividualOrder individualOrder) {
-        individualOrders.remove(individualOrder);
+    public void delete(int individualOrderId) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/delete/" + individualOrderId))
+                .DELETE().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() >= 300) {
+            throw new IOException("Error: " + response.statusCode() + " - " + response.body());
+        }
     }
 
 
