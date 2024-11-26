a15d251052a0981fc9c8493f63a972f19938480d
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2024-11-26 10:55:30 +0100

    :white_check_mark: ajout du findalldish PATCH  #136

--- common-library/src/main/java/commonlibrary/model/Dish.java
+++ common-library/src/main/java/commonlibrary/model/Dish.java
@@ -2,6 +2,7 @@ package commonlibrary.model;
 
 import commonlibrary.dto.DishDTO;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -9,6 +10,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@NoArgsConstructor
 public class Dish {
 
     private int id;
--- common-library/src/main/java/commonlibrary/repository/DishRepository.java
+++ common-library/src/main/java/commonlibrary/repository/DishRepository.java
@@ -1,13 +1,21 @@
 package commonlibrary.repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import commonlibrary.model.Dish;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class DishRepository {
     private final List<Dish> dishes = new ArrayList<>();
+    private static final String BASE_URL = "http://localhost:8082/";
+    private static final HttpClient client = HttpClient.newHttpClient();
 
     public Dish findById(int dishId) {
         return dishes.stream().filter(restaurant -> restaurant.getId() == dishId).findFirst().orElse(null);
@@ -21,11 +29,23 @@ public class DishRepository {
         dishes.remove(dish);
     }
 
-    public List<Dish> findAll() {
+    public List<Dish> findAll() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "dishes"))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        List<Dish> dishes = List.of(new ObjectMapper().readValue(response.body(), Dish[].class)); // throws exception if response is not a valid JSON array
         return dishes;
     }
 
     public void clear() {
         dishes.clear();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        DishRepository dishRepository = new DishRepository();
+        System.out.println(dishRepository.findAll());
     }
 }
