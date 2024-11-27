c8c0d5a9112660d42cfe826041f455f1d9c00a7d
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2024-11-27 10:45:44 +0100

    :white_check_mark: suborder repository  #136

--- common-library/src/main/java/commonlibrary/repository/SubOrderRepository.java
+++ common-library/src/main/java/commonlibrary/repository/SubOrderRepository.java
@@ -1,35 +1,85 @@
 package commonlibrary.repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import commonlibrary.model.Dish;
 import commonlibrary.model.order.SubOrder;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SubOrderRepository {
 
-    private final List<SubOrder> subOrders = new ArrayList<>();
+    private static final String BASE_URL = "http://localhost:8082/sub-orders";
+    private static final HttpClient client = HttpClient.newHttpClient();
 
-    public void add(SubOrder subOrder) {
-        subOrders.add(subOrder);
+
+    public void add(SubOrder subOrder) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/create"))
+                .POST(HttpRequest.BodyPublishers.ofString(new ObjectMapper().writeValueAsString(subOrder)))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() >= 300) {
+            throw new IOException("Error: " + response.statusCode() + " - " + response.body());
+        }
     }
 
-    public SubOrder findById(int id) {
-        return subOrders.stream().filter(subOrder -> subOrder.getId() == id).findFirst().orElse(null);
+    public SubOrder findById(int id) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/get/" + id))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() >= 300) {
+            throw new IOException("Error: " + response.statusCode() + " - " + response.body());
+        }
+        SubOrder subOrder = new ObjectMapper().readValue(response.body(), SubOrder.class);
+        return subOrder;
     }
 
-    public List<SubOrder> findAll() {
+    public List<SubOrder> findAll() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        if (response.statusCode() >= 300) {
+            throw new IOException("Error: " + response.statusCode() + " - " + response.body());
+
+        }
+        List<SubOrder> subOrders = List.of(new ObjectMapper().readValue(response.body(), SubOrder[].class)); // throws exception if response is not a valid JSON array
         return subOrders;
     }
 
-    public void delete(SubOrder subOrder) {
-        subOrders.remove(subOrder);
+    public void delete(int subOrderId) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/delete/" + subOrderId))
+                .DELETE().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() >= 300) {
+            throw new IOException("Error: " + response.statusCode() + " - " + response.body());
+        }
     }
 
-    public void clear() {
-        subOrders.clear();
-    }
+    public SubOrder findByUserId(int id) throws IOException, InterruptedException {
 
-    public SubOrder findByUserId(int id) {
-        return subOrders.stream().filter(subOrder -> subOrder.getUser().getId() == id).findFirst().orElse(null);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "?userId=" + id))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        if (response.statusCode() >= 300) {
+            throw new IOException("Error: " + response.statusCode() + " - " + response.body());
+
+        }
+        SubOrder subOrder = new ObjectMapper().readValue(response.body(), SubOrder.class); // throws exception if response is not a valid JSON array
+        return subOrder;
     }
 }
