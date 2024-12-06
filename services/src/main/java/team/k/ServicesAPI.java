e9d39b7629b08c91627a6bc7ba81bd1f4936999d
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-12-06 18:33:13 +0100

    :bug: fix service server base package #146

--- services/src/main/java/team/k/ServicesAPI.java
+++ services/src/main/java/team/k/ServicesAPI.java
@@ -4,7 +4,7 @@ import ssdbrestframework.SSDBHttpServer;
 
 public class ServicesAPI {
     public static void main(String[] args) {
-        SSDBHttpServer serv = new SSDBHttpServer(8083, "team.k.controllers");
+        SSDBHttpServer serv = new SSDBHttpServer(8083, "team.k.controller");
         serv.start();
     }
 }
