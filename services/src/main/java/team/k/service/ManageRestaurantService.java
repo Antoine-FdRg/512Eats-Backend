68d85059f9f26a88b05f18128151ad3ca7759c59
Roxane Bacon <roxane.bacon@etu.univ-cotedazur.fr>
2024-12-07 22:57:17 +0100

    ajout ManageRestaurantController in api

--- services/src/main/java/team/k/service/ManageRestaurantService.java
+++ services/src/main/java/team/k/service/ManageRestaurantService.java
@@ -1,6 +1,7 @@
 package team.k.service;
 
 import commonlibrary.model.Dish;
+import ssdbrestframework.SSDBQueryProcessingException;
 import team.k.repository.RestaurantRepository;
 import commonlibrary.model.restaurant.Restaurant;
 
@@ -10,7 +11,7 @@ import java.time.LocalTime;
 public class ManageRestaurantService {
     private static final String RESTAURANT_NOT_FOUND = "Restaurant not found";
 
-    public static Restaurant updateRestaurantInfos(int restaurantId, String openTime, String closeTime) {
+    public static Restaurant updateRestaurantInfos(int restaurantId, String openTime, String closeTime) throws SSDBQueryProcessingException {
         Restaurant restaurant = restaurantValidator(restaurantId);
         if (openTime != null) {
             restaurant.setOpen(LocalTime.parse(openTime));
@@ -21,7 +22,7 @@ public class ManageRestaurantService {
         return restaurant;
     }
 
-    public static Restaurant addDish(int restaurantId, String dishName, String dishDescription, double dishPrice, int dishPreparationTime) {
+    public static Restaurant addDish(int restaurantId, String dishName, String dishDescription, double dishPrice, int dishPreparationTime) throws SSDBQueryProcessingException {
         Restaurant restaurant = restaurantValidator(restaurantId);
         restaurant.addDish(new Dish.Builder()
                 .setName(dishName)
@@ -33,17 +34,17 @@ public class ManageRestaurantService {
         return restaurant;
     }
 
-    public static Restaurant removeDish(int restaurantId, int dishId) {
+    public static Restaurant removeDish(int restaurantId, int dishId) throws SSDBQueryProcessingException {
         Restaurant restaurant = restaurantValidator(restaurantId);
         restaurant.removeDish(dishId);
         return restaurant;
     }
 
-    public static Restaurant updateDish(int restaurantId, int dishId, double newDishPrice, int newDishPreparationTime) {
+    public static Restaurant updateDish(int restaurantId, int dishId, double newDishPrice, int newDishPreparationTime) throws SSDBQueryProcessingException {
         Restaurant restaurant = restaurantValidator(restaurantId);
         Dish dish = restaurant.getDishes().stream().filter(d -> d.getId() == dishId).findFirst().orElse(null);
         if (dish == null) {
-            throw new IllegalArgumentException("Dish not found");
+            throw new SSDBQueryProcessingException(404, "Dish not found");
         }
         if (newDishPrice != 0) {
             dish.setPrice(newDishPrice);
@@ -55,10 +56,10 @@ public class ManageRestaurantService {
 
     }
 
-    private static Restaurant restaurantValidator(int restaurantId) {
+    private static Restaurant restaurantValidator(int restaurantId) throws SSDBQueryProcessingException {
         Restaurant restaurant = RestaurantRepository.findById(restaurantId);
         if (restaurant == null) {
-            throw new IllegalArgumentException(RESTAURANT_NOT_FOUND);
+            throw new SSDBQueryProcessingException(404, RESTAURANT_NOT_FOUND);
         }
         return restaurant;
     }
